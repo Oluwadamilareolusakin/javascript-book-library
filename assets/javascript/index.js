34f85ca23d5189d0ec29eb54df35166a9c8d1737
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-11-14 09:02:13 +0100

    Add fetch books method

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -8,3 +8,21 @@ class Book {
     this.status = status;
   }
 }
+
+
+const fetchBooks = () =>{
+  const fetchedBooks = books.map((book) => {
+    return(
+      `<div class = 'book'>
+        <p>${book.title}</p>
+        <p>${book.author}</p>
+        <p>${book.genre}</p>
+        <p>${book.status}</p>
+      </div>`
+    )
+  });
+
+  return fetchedBooks;
+}
+
+
