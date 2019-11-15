ab87952a595e0d32367907372a78b916f5868c8d
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-11-15 18:29:03 +0100

    Fix lint errors

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,6 @@
 let books = [];
 
-class Book {
+class Book{
   constructor(title, author, genre, status){
     this.title = title;
     this.author = author;
@@ -9,19 +9,15 @@ class Book {
   }
 }
 
-
-
-const fetchBooks = () =>{
-  const fetchedBooks = books.map((book) => {
-    return(
-      `
-      <div class = 'column'>
+const fetchBooks = () => {
+  const fetchedBooks = books.map((book) =>
+      `<div class = 'column'>
         <div class = 'book'>
           <div class='overlay row'>
             <div class='column'>
               <i class = 'delete-icon fas fa-times-circle' onclick = 'deleteBooks(${book.id})'></i>
-              ${ book.status == 'Unread' ? 
-              `<button class='mark-as-read' onclick='markAsRead(${book.id})'>Read</button>` : `<button class='mark-as-read' onclick='markAsUnread(${book.id})'>Unread</button>`
+              ${ book.status === 'Unread'
+              ?`button class='mark-as-read' onclick='markAsRead(${book.id})'>Read</button>` : `<button class='mark-as-read' onclick='markAsUnread(${book.id})'>Unread</button>`
               }
             </div>
           </div>
@@ -33,34 +29,34 @@ const fetchBooks = () =>{
           <p><span>Status:</span> ${book.status}</p>
         </div>
       </div>`
-    )
-  });
-
+    
+  );
   return fetchedBooks.join('');
-}
+};
 
 const render = (template, node) => {
   if (!node) return;
   node.innerHTML = template;
-}
+};
 
 const setBooks = () =>{
   const allBooks = fetchBooks();
   const node = document.querySelector('.books');
   render(allBooks, node);
-}
+};
 
 
-const createBooks = () => {for(let i = 0; i <= 10; i++){
-  let book = new Book(`The boy who cried wolf ${i + 1}`, 'The man who saw the boy', 'A genre of tears', 'Unread' )
-  books.push(book);
-  book.id = books.indexOf(book);
-}
-setBooks();
+const createBooks = () => { 
+  for (let i = 0; i <= 10; i++) {
+    const book = new Book(`The boy who cried wolf ${i + 1}`, 'The man who saw the boy', 'A genre of tears', 'Unread' )
+    books.push(book);
+    book.id = books.indexOf(book);
+  };
+  setBooks();
 }
 
 const openFormModal = (e) => {
-  if (e.target.id != 'new-book-btn') return;
+  if (e.target.id !== 'new-book-btn') return;
   const bookModal = document.querySelector('#book-modal');
   bookModal.classList.toggle('book-form');
   bookModal.classList.toggle('closed-book-form');
@@ -74,14 +70,14 @@ const closeModal = (e) => {
 }
 
 const addBookToLibrary = (e) => {
-  if (e.target.id != 'form-submit') return;
+  if (e.target.id !== 'form-submit') return;
   e.preventDefault();
   const form = document.getElementById('book-form');
   const title = form.elements[0].value;
   const author = form.elements[1].value;
   const genre = form.elements[2].value;
   const status = form.elements[3].value;
-  let book = new Book(title, author, genre, status);
+  const book = new Book(title, author, genre, status);
   books.push(book);
   setBooks();
   book.id = books.indexOf(book);
@@ -104,3 +100,5 @@ const markAsUnread = (id) =>{
   book.status = 'Unread'
   setBooks();
 }
+
+createBooks();
